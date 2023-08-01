86d37e75efd800f0c021f6aa7ab738078a47250e
hftecnica1 <hernan.fig60@goutlook.com>
2023-08-01 18:10:48 +0000

    agregando un index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Base de página</title>
+</head>
+<body>
+    Hora de trabajar 🥶
+</body>
+</html>
